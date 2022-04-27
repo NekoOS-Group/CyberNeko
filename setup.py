f13dc838b03eda341ad8d63aad66832eadd4ab3f
bzy <bzy.mic@qq.com>
2022-04-27 18:47:54 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='CyberNeko',
+    version='0.0.1',
+    packages=['teleneko', 'teleneko.bits', 'teleneko.tests'],
+    url='https://github.com/NekoOS-Group/CyberNeko',
+    license='GPL-3.0 License',
+    author='bzy',
+    author_email='bzy.mic@qq.com',
+    description='first release'
+)
